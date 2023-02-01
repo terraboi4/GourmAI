350e73cf4136257d09ba4d8a3bc483850169df6a
Glitch (hexagonal-holy-kitty) <none>
2023-02-01 15:32:01 +0000

    📡🛵 Updated with Glitch

---
+++ js/form.js
@@ -0,0 +1,9 @@
+//form loading
+
+const form = [...document.querySelector('.form').children];
+
+form.forEach((item, i) => {
+    setTimeout(() => {
+        item.style.opacity = 1;
+    }, i*100);
+})
