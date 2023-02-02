0670f7561cb6c9fa73b9c7a45af170f51b36cf1f
terraboi4 <91034970+terraboi4@users.noreply.github.com>
2023-02-02 20:04:13 +0000

    make react app

--- public/js/form.js
+++
@@ -1,9 +0,0 @@
-//form loading
-
-const form = [...document.querySelector('.form').children];
-
-form.forEach((item, i) => {
-    setTimeout(() => {
-        item.style.opacity = 1;
-    }, i*100);
-})
