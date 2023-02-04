cfae2ec6638828ee8ba197ae5be07b96d4898f81
terraboi4 <jaxstar24@gmail.com>
2023-02-03 20:55:14 -0600

    📦 NEW: react boilerplate

---
+++ src/components/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard/>}>
+          </Route>
+          <Route path="/preferences" element={<Preferences/>}>
+            <Preferences />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
