60f303bef7cf7b9962aba64790c6323e7ab897c1
terraboi4 <jaxstar24@gmail.com>
2023-03-01 10:16:21 -0600

    ❌ REMOVE: checkbox component

--- src/lib/Checkbox.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    export let id;
-</script>
-
-<input type="checkbox" id="{id}">
-<label for="{id}">{id}</label>
--- src/routes/Dashboard.svelte
+++ src/routes/Dashboard.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import { pb, currentUser } from "../lib/pocketbase";
-    import Checkbox from "../lib/Checkbox.svelte";
     function signOut() {
         pb.authStore.clear();
         window.location.href = '/login'
@@ -31,7 +30,7 @@
                     <a href="/credits" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Buy Credits</a>
                 </li>
                 <li>
-                
+                {#if $currentUser}
                     <a href="/profile" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
                         <img
                         class="w-8 h-8 rounded-full"
@@ -39,6 +38,9 @@
                         alt="avatar"
                         />
                     </a>
+                {:else}
+                    <a href="/login">Sign Up</a>
+                {/if}
                 </li>
             </ul>
         </div>
@@ -48,33 +50,22 @@
 {#if gr_show}
     <div class="text-center">
 
-        <div class="tab hidden">
+        <div class="tab p-5">
             <h1 class="text-4xl p-5">Ingredients</h1>
             <input class="rounded-lg px-3 py-3 placeholder-gray-300 text-slate-600 relative bg-white text-sm border-0 shadow outline-none w-3/5" type="text" placeholder="apples, bananas, oranges...">
         </div>
-        <div class="tab hidden">
+        <hr>
+        <div class="tab p-5">
             <h1 class="text-4xl p-5">Dietary Restrictions</h1>
-            
-            <Checkbox id="Vegetarian"/>
-            <Checkbox id="Vegan"/>
-            <Checkbox id="Gluten-Free"/> <br>
-            <Checkbox id="Lactose Intolerance"/>
-            <Checkbox id="Ketogenic"/>
-            <Checkbox id="Paleolithic"/>
             <h1 class="text-2xl pt-5">Allergies</h1>
             <input class=" rounded-lg px-3 py-3 my-6 placeholder-gray-300 text-slate-600 relative bg-white text-sm border-0 shadow outline-none w-3/5" type="text" placeholder="Allergies">
         </div>
-        <div class="tab hidden">
+        <hr>
+        <div class="tab p-5">
             <h1 class="text-4xl p-5">Filters</h1>
             <div class="rounded-lg py-5">
-                <button class="inline-block px-6 bg-logo-color hover:bg-[#ed7e39] rounded-md py-2 mx-2">🍴</button>
-                <button class="inline-block px-6 bg-logo-color hover:bg-[#ed7e39] rounded-md py-2 mx-2">🍴🍴</button>
-                <button class="inline-block px-6 bg-logo-color hover:bg-[#ed7e39] rounded-md py-2 mx-2">🍴🍴🍴</button>
-                <button class="inline-block px-6 bg-logo-color hover:bg-[#ed7e39] rounded-md py-2 mx-2">🍴🍴🍴🍴</button>
-                <button class="inline-block px-6 bg-logo-color hover:bg-[#ed7e39] rounded-md py-2 mx-2">🍴🍴🍴🍴🍴</button>
                 <p>Fanciness meter - 🍴 means very easy to make, 🍴🍴🍴🍴🍴 means difficult</p>
             </div>
-            <hr>
             <div class="rounded-lg py-5">
                 <button class="inline-block px-6 bg-logo-color hover:bg-[#ed7e39] rounded-md py-2 mx-2">Breakfast</button>
                 <button class="inline-block px-6 bg-logo-color hover:bg-[#ed7e39] rounded-md py-2 mx-2">Lunch</button>
@@ -84,21 +75,6 @@
             </div>
         </div>
 
-        <!--buttons-->
-        <div class="overflow-auto">
-            <div class="float-right">
-                <button type="button" id="prevBtn" on:click={formAction(-1)}>Previous</button>
-                <button type="button" id="nextBtn" on:click={formAction(1)}>Next</button>
-            </div>
-        </div>
-        
-        <!--step things-->
-        <div class="text-center mt-10">
-            <span class="h-[15] w-[15] my-1 bg-[bg-gray-300] rounded-lg inline-block opacity-50 active:opacity-100"></span>
-            <span class="h-[15] w-[15] my-1 bg-[bg-gray-300] rounded-lg inline-block opacity-50 active:opacity-100"></span>
-            <span class="h-[15] w-[15] my-1 bg-[bg-gray-300] rounded-lg inline-block opacity-50 active:opacity-100"></span>
-            <span class="h-[15] w-[15] my-1 bg-[bg-gray-300] rounded-lg inline-block opacity-50 active:opacity-100"></span>
-        </div>
 
     </div>
 
